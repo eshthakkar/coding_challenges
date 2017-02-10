e94bf03a7d160741b20e1d906774f99e3cb1774d
vagrant <eshthakkar@gmail.com>
2017-02-10 02:05:01 +0000

    Function to convert a decimal number to its binary equivalent

--- str_to_num.py
+++ str_to_num.py
@@ -11,7 +11,7 @@ def str_to_num(string_num):
     """
 
     val = 0
-    
+
     if string_num is None or string_num == "":
         raise TypeError("String to be converted is empty or Null")
 
@@ -28,6 +28,32 @@ def str_to_num(string_num):
         val = val * 10 + (ord(char) - ord("0"))
 
     return val 
+
+
+def dec_to_bin(num):
+    """ Decimal to binary conversion
+        >>> print dec_to_bin(5)
+        101
+
+        >>> print dec_to_bin(2)
+        10
+    """
+
+    count = 0
+    out = ""
+
+    if num == 0 or num == 1:
+        return num
+
+    while (num > 1):
+        rem = num % 2
+        num = num / 2
+        out = str(rem * 10**count) + out
+        count += 1
+        
+    out = str(num) + out    
+    return out    
+
 
 
 if __name__ == "__main__":
