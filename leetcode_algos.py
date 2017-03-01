a061fd8b10b1c70b55196ffe45e9129ee10e8ed5
vagrant <eshthakkar@gmail.com>
2017-03-01 07:12:36 +0000

    remove duplicates from sorted array and return the new length of array

---
+++ leetcode_algos.py
@@ -0,0 +1,44 @@
+def remove_duplicates(nums):
+    """Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length.
+
+    Do not allocate extra space for another array, you must do this in place with constant memory.
+
+    For example,
+    Given input array nums = [1,1,2],
+
+    Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively. It doesn't matter what you leave beyond the new length.
+
+    >>> nums = [1, 1, 1]
+    >>> remove_duplicates(nums)
+    1
+
+    >>> nums = [1, 1, 2]
+    >>> remove_duplicates(nums)
+    2
+
+    >>> nums = []
+    >>> remove_duplicates(nums)
+    0
+
+    """
+
+    if not nums:
+        return 0
+            
+    count = 0
+    for i in xrange(1,len(nums)):
+        if nums[i] != nums[count]:
+            count += 1
+            nums[count] = nums[i]
+            
+    return count + 1 
+
+
+if __name__ == "__main__":
+    import doctest
+
+    print
+    result = doctest.testmod()
+    if not result.failed:
+        print "ALL TESTS PASSED. GOOD WORK!"
+    print            
