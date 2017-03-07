8fa20f24b4987df52570e60e2057276afdf1698a
vagrant <eshthakkar@gmail.com>
2017-03-07 23:03:00 +0000

    Recursive DFS search to find if the target word can be made out of the words provided in a list

---
+++ word_probs.py
@@ -0,0 +1,81 @@
+# Time to lookup a word from the set is O(1) and total runtime is O(2^n) where n is the length of the target word
+def is_word_possible(vocab, target_word, start):
+    """ Check if the given word can be made out of the provided list of words
+        >>> vocab = ["cats", "dogs"]
+        >>> print is_word_possible(vocab, "catsdogs", 0)
+        True
+
+        >>> print is_word_possible(vocab, "tower", 0)
+        False
+
+        >>> print is_word_possible(vocab, "catstower", 0)
+        False
+
+        >>> new_vocab = ["to", "tow", "er"]
+        >>> print is_word_possible(new_vocab, "tower", 0)
+        True
+
+        >>> print is_word_possible([], "aa", 0)
+        False
+
+        >>> v_vocab = ["a", "b", "c", "d"]
+        >>> print is_word_possible(v_vocab, "abd", 0)
+        True
+
+        >>> print is_word_possible(v_vocab, "c", 0)
+        True
+
+        >>> print is_word_possible(v_vocab, "d", 0)
+        True
+
+        >>> print is_word_possible(v_vocab, "dc", 0)
+        True
+
+        >>> print is_word_possible(v_vocab, "ddddddcccccbbbbaaaaa", 0)
+        True
+
+
+    """
+
+    vocab_set = set(vocab)
+
+    if not vocab_set:
+        return False 
+
+    if start == len(target_word):
+        return True
+
+    if target_word in vocab_set:
+        return True    
+
+    for i in xrange(0,len(target_word)):
+
+        # Debugging
+        # print "start: ", start
+        # print "i: " , i
+        # print "Target_word_slice: " , target_word[start:start+i]
+
+        if target_word[start:start + i] in vocab_set:
+            if is_word_possible(vocab, target_word, start + i):
+                return True
+
+    return False        
+
+
+if __name__ == "__main__":
+    import doctest
+
+    print
+    result = doctest.testmod()
+    if not result.failed:
+        print "ALL TESTS PASSED. GOOD WORK!"
+    print            
+
+
+
+
+
+
+
+
+
