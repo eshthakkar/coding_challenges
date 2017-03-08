bf39265d0da0e1b0e9c7a1345def3cd8e1791036
vagrant <eshthakkar@gmail.com>
2017-03-08 00:08:25 +0000

    Added a cache which optimizes the runtime such that paths that have returned False are not checked again

--- word_probs.py
+++ word_probs.py
@@ -1,5 +1,6 @@
 # Time to lookup a word from the set is O(1) and total runtime is O(2^n) where n is the length of the target word
-def is_word_possible(vocab, target_word, start):
+# Added a cache set seen to not go over paths that have already returned false
+def is_word_possible(vocab, target_word, start, seen=None):
     """ Check if the given word can be made out of the provided list of words
         >>> vocab = ["cats", "dogs"]
         >>> print is_word_possible(vocab, "catsdogs", 0)
@@ -39,6 +40,9 @@ def is_word_possible(vocab, target_word, start):
 
     vocab_set = set(vocab)
 
+    if seen is None:
+        seen = set()
+
     if not vocab_set:
         return False 
 
@@ -48,17 +52,18 @@ def is_word_possible(vocab, target_word, start):
     if target_word in vocab_set:
         return True    
 
-    for i in xrange(0,len(target_word)):
+    for i in xrange(0,len(target_word) - start + 1):
 
         # Debugging
+        # print seen
         # print "start: ", start
         # print "i: " , i
         # print "Target_word_slice: " , target_word[start:start+i]
 
-        if target_word[start:start + i] in vocab_set:
-            if is_word_possible(vocab, target_word, start + i):
+        if target_word[start:start + i] in vocab_set and start + i not in seen:
+            if is_word_possible(vocab, target_word, start + i, seen):
                 return True
-
+            seen.add(start + i)    
     return False        
 
 
